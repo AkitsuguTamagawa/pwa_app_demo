1ba35f5a1bbe51fa006f6683b62768d67ff60650
AkitsuguTamagawa <tamagawa@spacemarket.co.jp>
2019-02-05 11:50:33 +0900

    initial commit

---
+++ pwa_app_demo/app/src/main/java/exsample/com/demo_pwa/launchScreen.kt
@@ -0,0 +1,12 @@
+package exsample.com.demo_pwa
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class launchScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launch_screen)
+    }
+}
